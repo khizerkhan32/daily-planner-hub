7bce6c7f496d263920555e24e9a5c4562ef2de8f
Syed Muhammad Ahmed <ssyedahmed31@gmail.com>
2026-03-12 15:34:20 +0500

    done

--- backend/models/Category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const categorySchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: String, required: true },
-  color: { type: String, default: '#FFFFFF' },
-  is_default: { type: Boolean, default: false }
-});
-categorySchema.index({ user_id: 1 });
-module.exports = mongoose.model('Category', categorySchema);
--- backend/models/TaskTemplate.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const templateSchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: String, required: true },
-  tasks_json: { type: String, required: true }  // JSON string of task array
-});
-module.exports = mongoose.model('TaskTemplate', templateSchema);
